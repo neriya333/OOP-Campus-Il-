498f2a90990a367811326be27d3895fd96c3432c
Neriya Cohen <51446652+neriya333@users.noreply.github.com>
2023-01-22 12:50:22 +0200

    Update Enums.java

--- javaLang/Enums.java
+++ javaLang/Enums.java
@@ -1,4 +1,54 @@
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ENUM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~--------~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  ENUM  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~--------~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+/**
+ * I learned that Math.PI is the usage
+ * 
+ * that each of enum options is basicly a class.
+ * the simple use is to creaet a class and if == used for measring if two objects are of the same class.
+ * exmple: look at those line, that prints false. 
+ * 
+    EqualSidedShapes p = EqualSidedShapes.TRIANGLE; 
+    System.out.println(p==EqualSidedShapes.PENTAGON);
+ * 
+ * 
+ * BUT Emun can hold more than only is the class the same.
+ * as shown above having functions and initializer.
+ */
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ START EX 1 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+enum Days{
+    SUNDAY("OOP!"), MONDAY("NAND"), TUESDAY("Probability"),
+    WEDNESDAY("OOP!"), THURSDAY("Algorithms"), FRIDAY("It's Friday!"),
+    SHABBAS("Shabbat Hayom!"); 
+    
+    // each constant is made once and calls the constructor
+    
+    Days(String dayClass) // constructor for all the constants
+    {
+        this.dayClass = dayClass;
+    }
+    private String dayClass; // private member
+    public String getClass(){ // public getter for the member
+        return dayClass;
+    }
+}
+
+class Main{
+    public static void main(String[] args){
+        for(Days day : Days.values()){ // for each loop
+            System.out.println(day.get_class());
+        }
+    }
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ END OF EX 1 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
 public class ShapeOperator {
 
 
@@ -40,18 +90,3 @@ enum EqualSidedShapes{
     public int getNumOfEdges(){return this.numEdges;}
     
 }
-
-/**
- * I learned that Math.PI is the usage
- * 
- * that each of enum options is basicly a class.
- * the simple use is to creaet a class and if == used for measring if two objects are of the same class.
- * exmple: look at those line, that prints false. 
- * 
-    EqualSidedShapes p = EqualSidedShapes.TRIANGLE; 
-    System.out.println(p==EqualSidedShapes.PENTAGON);
- * 
- * 
- * BUT Emun can hold more than only is the class the same.
- * as shown above having functions and initializer.
- */
